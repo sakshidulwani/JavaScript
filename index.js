225f9f25d150183160d4e4e17a5d27f28c4ee564
sakshidulwani <dulwanisakshi@gmail.com>
2021-01-02 21:53:21 +0530

    Changes

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
 
 
-let highIncome  = false ;
-let goodCreditScore = false;
-let eligibleForLoan = highIncome || goodCreditScore;
-
-console.log('Eligible' ,eligibleForLoan);
-
-let applicationRefused = !eligibleForLoan;
-console.log('Application Refused' , applicationRefused);
