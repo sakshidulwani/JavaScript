0d671cd2de4bad0c705a25b132d7204b6e5ba217
sakshidulwani <dulwanisakshi@gmail.com>
2021-01-02 21:08:36 +0530

    Added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // If a customer has more than 100 points
 // they are a 'gold' customer , otherwise,
-// they are a 'silver' customer .
+
 
 let points = 90;
 let type = points > 100 ? 'gold ' : 'silver';
