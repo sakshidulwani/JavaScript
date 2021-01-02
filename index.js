a0a2f7aca03464c4767b3443ffc3a52f8ed43c54
sakshidulwani <dulwanisakshi@gmail.com>
2021-01-02 20:50:24 +0530

    Added

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
+let x = 20;
+let y = 5;
 
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x **y);
+
+console.log(x++);
+console.log(x);
+
+console.log(--x)
