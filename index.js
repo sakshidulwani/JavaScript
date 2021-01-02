a915f85a5f67da83a5c871e8b75282adcf955a54
sakshidulwani <dulwanisakshi@gmail.com>
2021-01-02 21:04:34 +0530

    Changes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-
+// Strict Equality (Type + Value)
 console.log( 1 === 1);
 console.log('1' === 1);
 
+
+//Lose Equality (Value)
 console.log (1 == 1);
 console.log('1' == '1');
 console.log(true == 1);
