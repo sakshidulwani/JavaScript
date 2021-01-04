b8079e1aadce66da1efbfa40f3a9c84f44f96bed
sakshidulwani <dulwanisakshi@gmail.com>
2021-01-04 12:53:06 +0530

    changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const writePermission = 2;
 const executePermission = 2;
 
 let myPermission = 0;
-myPermission = myPermission | readPermission | writePermission;
+myPermission = myPermission | writePermission;
 
 let message = (myPermission & readPermission) ? "yes" : "no";
 
