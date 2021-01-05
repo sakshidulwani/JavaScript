087cc4f768ba48fe6ccddc587198ff99e960b67a
sakshidulwani <dulwanisakshi@gmail.com>
2021-01-05 12:56:11 +0530

    Added

--- Loops/for-in.js
+++ Loops/for-in.js
@@ -6,3 +6,8 @@ const person = {
 
 for (let key in person) 
    console.log(key , person[key]);
+
+   const colors = ['red' , 'green' , 'blue']
+
+   for (let index in colors)
+   console.log(index , colors[index]);
