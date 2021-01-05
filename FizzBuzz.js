1013d980544c6de950ec874afe5b0d269a514945
sakshidulwani <dulwanisakshi@gmail.com>
2021-01-05 20:07:35 +0530

    Added

---
+++ FizzBuzz.js
@@ -0,0 +1,19 @@
+
+const output = fizzBuzz(false);
+console.log(output);
+
+function fizzBuzz(input) {
+    if(typeof input !== "number")
+    return "Not a number";
+
+    if (input % 3 === 0) 
+    return "Fizz";
+
+    if(input % 5 === 0)
+    return "Buzz";
+
+    if( ( input % 3 === 0) && (input % 5 === 0))
+    return "FizzBuzz";
+
+    return input;
+}
