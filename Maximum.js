fcd618bd073cc32686d24ab5d74bf5ba50436a8a
sakshidulwani <dulwanisakshi@gmail.com>
2021-01-05 13:40:45 +0530

    Added

---
+++ Maximum.js
@@ -0,0 +1,7 @@
+
+function max (a , b) {
+    return (a>b) ? a : b;
+}
+
+let number = max(2,3)
+console.log(number);
--- break_continue.js
+++ break_continue.js
@@ -2,9 +2,13 @@
 
 let i = 0;
 while (i <= 10) {
-    if ( i === 5) 
-    break;
+    // if ( i === 5) 
+    // break;
+    if (i % 2 === 0){
+        i++;
+        continue;
+    }
 
     console.log(i);
-    i++;
+    i++; 
 }
