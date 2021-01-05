a8f74c72b21bebfc09303964b1fb9f3896e0fea4
sakshidulwani <dulwanisakshi@gmail.com>
2021-01-05 19:59:02 +0530

    changes

---
+++ Landscape_portrait.js
@@ -0,0 +1,7 @@
+
+function isLandscape(width , height) {
+    return (width > height);
+  
+}
+
+console.log(isLandscape(500,800));
--- Maximum.js
+++ Maximum.js
@@ -3,5 +3,5 @@ function max (a , b) {
     return (a > b) ? a : b;
 }
 
-let number = max(5,10)
+let number = max(5 , 10)
 console.log(number);
