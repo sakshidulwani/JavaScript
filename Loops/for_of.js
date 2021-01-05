db3e3103c834b534e80f7c6f54c1f532956549fa
sakshidulwani <dulwanisakshi@gmail.com>
2021-01-05 13:06:06 +0530

    changes

---
+++ Loops/for_of.js
@@ -0,0 +1,12 @@
+const person = {
+    name : "Sakshi",
+    age : 20,
+}
+
+for (let key in person) 
+   console.log(key , person[key]);
+
+   const colors = ['red' , 'green' , 'blue']
+
+   for (let color in colors)
+   console.log(colors);
